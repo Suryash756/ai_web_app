13582658dd68bfe7762039ca55b60f206b119667
Suryash756 <86527278+Suryash756@users.noreply.github.com>
2023-08-26 08:41:30 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0;
 leftWristY= 0;
 rightWristX =0;
 rightWristY =0;
+scoreLeftWrist =0;
+song_status = "";
+song1_status ="";
 function preload()
 {
     song = loadSound("music.mp3");
@@ -28,8 +31,13 @@ function gotPoses(results)
     if(results.length>0)
     {
         console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = "+scoreLeftWrist);
+        console.log("scoreRightWrist = "+scoreRightWrist);
         leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
         console.log(" leftWristX = "+leftWristX +" , leftWristY = "+leftWristY);
 
         rightWristX = results[0].pose.rightWrist.x;
@@ -47,6 +55,37 @@ function modelLoaded()
 function draw()
 {
     image(video,0,0,600,500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    song_status = song.isPlaying();
+    song1_status = song1.isPlaying();
+                                                    
+    if(scoreLeftWrist>0.2){
+    circle(leftWristX,leftWristY,20);
+    song1.stop();
+
+    if(song_status == false)
+    {
+        song.play();
+        document.getElementById("play").innerHTML ="playing  Harry poter theme song ";
+    
+    }
+                        
+    }
+    if(scoreRightWrist>0.2){
+        circle(rightWristX,rightWristY,20);
+        song.stop();
+    
+        if(song1_status == false)
+        {
+            song1.play();
+            document.getElementById("play").innerHTML ="playing  game of thorns ";
+        
+        }
+                            
+        }
 }
 
 function play()
